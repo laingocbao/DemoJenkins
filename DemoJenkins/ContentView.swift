ed1340fa99f5736542c957ea3d7cf6b4ba304d74
Lại Ngọc Bảo <baoln@imt-soft.com>
2020-05-26 11:05:25 +0700

    Init Repo

---
+++ DemoJenkins/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoJenkins
+//
+//  Created by laingocbao on 5/26/20.
+//  Copyright © 2020 IMT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
